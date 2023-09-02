ecd932f3d34b03c0190de935cd6245e817b21114
cesarAlta <rces0365@gmail.com>
2023-09-02 18:18:19 -0300

    sdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
-import {
-  APP_BASE_HREF,
-} from '@angular/common';
+import { APP_BASE_HREF, HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HomeModule } from './modules/home/home.module';
 import { NavComponent } from './components/nav/nav.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, ModalComponent, FooterComponent],
-  imports: [BrowserModule, HomeModule, AppRoutingModule],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    ModalComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    HomeModule,
+    AppRoutingModule,
+  ],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' },
+  { provide: LocationStrategy, useClass: HashLocationStrategy }
+],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
